6775fd371e86387f2cedfc830e681f50be13b030
jjbeto <jjbeto@gmail.com>
2022-05-21 12:03:02 +0200

    base code from spring initializr

---
+++ src/main/java/com/jjbeto/microservice/iplocator/IpLocatorApplication.java
@@ -0,0 +1,13 @@
+package com.jjbeto.microservice.iplocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpLocatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpLocatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jjbeto/microservice/iplocator/IpLocatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjbeto.microservice.iplocator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpLocatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
