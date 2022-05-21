a864cab3e38edc6d94e46240988373ba76bf3acf
jjbeto <jjbeto@gmail.com>
2022-05-21 22:34:56 +0200

    add caching (ehcache)

--- src/main/java/com/jjbeto/microservice/iplocator/IpLocatorApplication.java
+++ src/main/java/com/jjbeto/microservice/iplocator/IpLocatorApplication.java
@@ -2,14 +2,16 @@ package com.jjbeto.microservice.iplocator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableCaching
 public class IpLocatorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IpLocatorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(IpLocatorApplication.class, args);
+    }
 
 }
--- src/main/java/com/jjbeto/microservice/iplocator/service/IpApiService.java
+++ src/main/java/com/jjbeto/microservice/iplocator/service/IpApiService.java
@@ -2,11 +2,16 @@ package com.jjbeto.microservice.iplocator.service;
 
 import com.jjbeto.microservice.iplocator.client.IpApiClient;
 import com.jjbeto.microservice.iplocator.dto.IpApiResponseDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class IpApiService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(IpApiService.class);
 
     private final IpApiClient ipApiClient;
 
@@ -24,11 +29,14 @@ public class IpApiService {
         this.fallbackCurrency = fallbackCurrency;
     }
 
+    @Cacheable(value = "IpApiService.getCurrency")
     public String getCurrency(final String ip) {
         final IpApiResponseDto dto = ipApiClient.getFromJsonByIp(ip, fields);
         if (dto == null || dto.getCurrency() == null) {
+            LOGGER.warn("Ip '{}' not found, currency: '{}' returned (fallback)", ip, fallbackCurrency);
             return fallbackCurrency;
         }
+        LOGGER.info("Ip '{}' found, currency: '{}'", ip, dto.getCurrency());
         return dto.getCurrency();
     }
 
